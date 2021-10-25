93e80856bc9c53522b84351fd98392c85a6e5cc8
Kevin Goldsmith <kevin@unitcircle.com>
2021-10-24 21:30:29 -0700

    partial checkin for adding more options for the report

--- spotify_report.py
+++ spotify_report.py
@@ -1,17 +1,46 @@
+import argparse
 from collections import Counter
-import datetime
+from datetime import date, timedelta
 import json
 import os
 from etlutils import datafiles
 
-last_saved = datafiles.find_newest_saved_month('data', datetime.datetime.now().year, 'spotify_tracks')
-filename = datafiles.get_monthly_file_path('data', 'spotify_tracks', last_saved[0], last_saved[1])
 
-if os.path.exists(filename):
-    with open(filename, 'r') as f:
-        played_tracks = json.load(f)
-else:
-    print(f'Error loading file {filename}')
+def get_played_tracks(timespan):
+    today = date.today()
+
+    if timespan == 'year':
+        start_day = date(today.year, 1, 1)
+    elif timespan == 'month':
+        start_day = date(today.year, today.month, 1)
+    elif timespan == 'week':
+        start_day = today - timedelta(days=(today.weekday()+1) % 7)
+    elif timespan == 'date':
+        start_day = today
+    else:
+        print(timespan)
+        return []
+
+    last_saved = datafiles.find_newest_saved_month('data', today.year, 'spotify_tracks')
+    filename = datafiles.get_monthly_file_path('data', 'spotify_tracks', last_saved[0], last_saved[1])
+
+    if os.path.exists(filename):
+        with open(filename, 'r') as f:
+            played_tracks = json.load(f)
+    else:
+        print(f'Error loading file {filename}')
+
+    return played_tracks
+
+
+parser = argparse.ArgumentParser(description="generate a report from saved Spotify plays")
+parser.add_argument('--span', help='set the timespan (day, week, month, year)', action='store', nargs=1, default='month', choices=['day', 'week', 'month', 'year'])
+parser.add_argument('--top', help='set the number for the artist/track/album count', action='store', nargs=1, default=10, type=int)
+args = parser.parse_args()
+top_count = int(args.top[0])
+timespan = args.span[0]
+
+played_tracks = get_played_tracks(timespan)
 
 artists = {}
 tracks = {}
@@ -31,9 +60,9 @@ track_items = Counter(track_list)
 sorted_track_items = sorted(track_items.items(), key=lambda pair: pair[1], reverse=True)
 
 print('ARTISTS')
-for item in sorted_artist_items[:10]:
+for item in sorted_artist_items[:top_count]:
     print(f"{artists[item[0]]['name']}, plays {item[1]}")
 
 print('\n\nTRACKS')
-for item in sorted_track_items[:10]:
+for item in sorted_track_items[:top_count]:
     print(f"{tracks[item[0]]}, plays {item[1]}")
