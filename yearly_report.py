54853c40476405edc43cdcf3224a1384e03c2323
Kevin Goldsmith <kevin@unitcircle.com>
2021-12-23 00:51:47 -0800

    added CSV of monthly plays for the top 20 artists

--- yearly_report.py
+++ yearly_report.py
@@ -2,6 +2,7 @@
 import argparse
 from collections import Counter
 import configparser
+import csv
 from datetime import datetime
 import json
 import logging
@@ -44,6 +45,7 @@ artists = {}
 albums = {}
 tracks = {}
 track_list = [] # tracks are for the year, the monthly isn't that interesting
+artist_year_list = []
 
 for month in range(1,13):
     month_file = datafiles.get_monthly_file_path('data', 'spotify_tracks', year, month)
@@ -61,7 +63,8 @@ for month in range(1,13):
         album_list = []
         for track in month_data['tracks']:
             for artist in track['track']['artists']:
-                artists[artist['id']] = artist
+                if not artist['id'] in artists:
+                    artists[artist['id']] = artist
                 artist_list.append(artist['id'])
 
             albums[track['track']['album']['id']] = track['track']['album']
@@ -70,8 +73,13 @@ for month in range(1,13):
             tracks[track['track']['id']] = track['track']['name']
             track_list.append(track['track']['id'])
         
+        artist_year_list.extend(artist_list)
         artist_items = Counter(artist_list)
         sorted_artist_items = sorted(artist_items.items(), key=lambda pair: pair[1], reverse=True)
+        for artist_iter in sorted_artist_items:
+            if not 'plays_by_month' in artists[artist_iter[0]]:
+                artists[artist_iter[0]]['plays_by_month'] = [0]*12
+            artists[artist_iter[0]]['plays_by_month'][month-1] = artist_iter[1]
 
         album_items = Counter(album_list)
         sorted_album_items = sorted(album_items.items(), key=lambda pair: pair[1], reverse=True)
@@ -82,11 +90,14 @@ for month in range(1,13):
     else:
         logger.info(f"missing month: {month}")
 
+top_artist_ids_for_year = []
+
 # monthly data
 for month in months:
     print(f"MONTH: {month['month']}")
     print(f"tracks played: {len(month['tracks'])}")
     print("\tTOP ARTISTS:")
+    top_artist_ids_for_year.extend(month['top_artists'])
     for artist in month['top_artists']:
         print(f"\t\t{artists[artist[0]]['name']}, plays {artist[1]}")
     print("\tTOP ALBUMS:")
@@ -99,3 +110,16 @@ sorted_track_items = sorted(track_items.items(), key=lambda pair: pair[1], rever
 print('\n\nTRACKS')
 for item in sorted_track_items[:top_count]:
     print(f"{tracks[item[0]]}, plays {item[1]}")
+
+artist_year_items = Counter(artist_year_list)
+sorted_artist_year_items = sorted(artist_year_items.items(), key=lambda pair: pair[1], reverse=True)
+for top_artist in sorted_artist_year_items[:20]:
+    print(f"{artists[top_artist[0]]['name']} ({top_artist[1]}): {artists[top_artist[0]]['plays_by_month']}")
+
+with open(f'top-artists-{year}.csv', 'w') as f:
+    writer = csv.writer(f, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+    writer.writerow(['artist', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'])
+    for top_artist in sorted_artist_year_items[:20]:
+        row = [artists[top_artist[0]]['name']]
+        row.extend(artists[top_artist[0]]['plays_by_month'])
+        writer.writerow(row)
